0c5dfe9ba168db7e84a00420c7785abbc7492cc0
prolific_conor <conor@prolificinteractive.com>
2014-04-07 18:00:52 -0400

    fixed default db name

--- Config/config.default.php
+++ Config/config.default.php
@@ -7,7 +7,7 @@ include_once __DIR__ . '/../mabi/MongoDataConnection.php';
 $app->addDataConnection(
   'default',
   MABI\MongoDataConnection::create(
-    'mongodb://localhost:27017/defaultDB'
+    'mongodb://user:password@localhost:27017/defaultDB'
   )
 );
 
